9ddce0d0a7828fa77a5b109c1496b0168ba9de2b
Matheuzk <mateo_valencia23212@elpoli.edu.co>
2026-04-01 17:06:59 -0500

    Fix Astro Tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   integrations: [
     react(),
-    tailwind(),
   ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   devToolbar: {
     enabled: false,
   },
