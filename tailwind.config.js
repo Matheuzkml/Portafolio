e9bcfdd5f44b75a1b5bea8ab039186a7bc3c3762
Matheuzk <mateo_valencia23212@elpoli.edu.co>
2026-04-01 15:11:42 -0500

    Primer commit del portafolio

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"DM Sans"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
